1547700880e1dffbb74ae26772882c709770cd03
dawood677 <dawoodumarani14@gmail.com>
2026-04-25 14:07:41 +0530

    Update greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Dawood Welcome To The World Of Devops  !");
+        System.out.println("Hello, Dawood Welcome To The World Of MulltiCloudWith Devops !");
     }
 }

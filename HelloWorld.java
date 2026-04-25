c748e278b06e5250f1e0f987ecad356e3683d1ba
dawood677 <dawoodumarani14@gmail.com>
2026-04-25 15:26:06 +0530

    Simplify greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Dawood Welcome To The World Of MulltiCloudWith Devops with AI........ !");
+        System.out.println("Hello, Dawood Welcome To The World Of Devops !");
     }
 }
